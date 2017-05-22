37dbc8138e9b7131922531fcd3a23210b417deed
Varun Khaneja <ca.aawc@yahoo.com>
2017-05-22 10:42:45 -0700

    Bring back the <time> ago and the HREF link for each message

--- chromium-utc-to-local-time.js
+++ chromium-utc-to-local-time.js
@@ -12,23 +12,23 @@
     var y = ', ' + new Date().getFullYear();
     var j = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     var m = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    var re = /(\((.* UTC)\))/;
+    var re = /(.*)\((.* UTC)\)(.*)/;
 
     [].forEach.call(document.querySelectorAll(
         '#messages tr.comment_title td:nth-of-type(3)'), function (el, i) {
         if (i === 0) {
             el.width = 160;
         } else {
-            var found = el.textContent.match(re);
+            var found = el.innerHTML.replace(/[\r\n]+/g," ").match(re);
             if (found && found[2]) {
                 var d = new Date(found[2]);
-                el.textContent = j[d.getDay()] + ' ' + m[d.getMonth()] + ' ' +
+                el.innerHTML = found[1] + '(' +
+                    j[d.getDay()] + ' ' + m[d.getMonth()] + ' ' +
                     (d.getDate() < 10 ? '0' : '') + d.getDate() + ', ' +
-                    (d.getHours() > 12 ? 
-                     (d.getHours() - 12 === 0 ? '12' : d.getHours() - 12) :
-                     (d.getHours() === 0 ? '12' : d.getHours())) +
-                    ':' + (d.getMinutes() < 10 ? '0' : '') + d.getMinutes() +
-                    ' ' + (d.getHours() > 12 ? 'p' : 'a') + 'm';
+                    (d.getHours() > 12 ? (d.getHours() - 12 === 0 ? '12' : d.getHours() - 12) :
+                     (d.getHours() === 0 ? '12' : d.getHours())) + ':' +
+                    (d.getMinutes() < 10 ? '0' : '') + d.getMinutes() + ' ' +
+                    (d.getHours() > 12 ? 'p' : 'a') + 'm)' + found[3];
             }
         }
     });
